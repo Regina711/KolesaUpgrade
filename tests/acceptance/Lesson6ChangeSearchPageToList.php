c55bd0defe158ee0a7e66865bc67a8e0690454cf
Regina Nassyrova <nassyrova.r@gmail.com>
2021-05-31 17:58:41 +0600

    Lesson 6. Using PageObject pattern

---
+++ tests/acceptance/Lesson6ChangeSearchPageToList.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Класс для проверки смены раскладки результатов поиска c Grid на List
+ */
+ 
+class Lesson6ChangeSearchPageToList
+{
+	/*
+	 *Проверяет сменe раскладки результатов поиска c Grid на List
+	 */
+    public function checkChangeSearchPageToList(AcceptanceTester $I)
+    {
+    }
+}
