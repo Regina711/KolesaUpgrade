ca71e39f8cd9879ec902d50e86a915b4289189ac
Regina Nassyrova <nassyrova.r@gmail.com>
2021-05-31 17:18:56 +0600

    Lesson 5. Adding CSS and Xpath selectors

--- tests/acceptance/Lesson5AcceptanceCest.php
+++ tests/acceptance/Lesson5AcceptanceCest.php
@@ -9,6 +9,12 @@ class Lesson5AcceptanceCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
+		$blouseCardCss = '#homefeatured > li:nth-child(2)';
+		$blouseCardXPath = '//li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 last-item-of-mobile-line']';
+		$quickViewCss = '#homefeatured > li:nth-child(2) a.quick-view';
+		$quickViewXPath = '//a[@class='quick-view' and contains(@href, 'id_product=2&controller=product')]';
+		$blouseModalWindowCss = '#product > div';
+		$blouseModalWindowXPath = '//*[@id="product"]'; 
 		$I->amOnPage('');
 		$I->seeElement('#homefeatured > li:nth-child(2)');
 		$I->click('#homefeatured > li:nth-child(2) a.product_img_link');
