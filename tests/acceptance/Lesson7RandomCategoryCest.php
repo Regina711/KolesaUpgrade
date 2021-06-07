95faf3a2bb7559bf6bcdbe4a28ac8bf6f1bb1c49
Regina Nassyrova <nassyrova.r@gmail.com>
2021-06-07 20:04:01 +0600

    Lesson 7 Тест на проверку выбора 2 рандомных элементов из категорий на habr

--- tests/acceptance/Lesson7RandomCategoryCest.php
+++ tests/acceptance/Lesson7RandomCategoryCest.php
@@ -1,17 +1,48 @@
 <?php
 
-namespace Tests\Acceptance;
+//use Page\Categories;
+
 /**
- * Класс для выбора 2 рандомных элементов из категорий на habr 
+ * Тест на проверку выбора 2 рандомных элементов из категорий на habr 
+ * @group test
+ * @param Example $data
+ * @dataProvider getDataForCategory
  */
  
 class Lesson7RandomCategoryCest
 {
+	
+	public $categories = [
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/top/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/feed/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/flows/develop/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/flows/admin/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/flows/design/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/flows/management/"]',
+		'//li[@class="nav-links__item"]/a[@href="https://habr.com/ru/flows/marketing/"]'
+	];
+	
 	/*
 	 * 
 	 */
     public function chooseRandomCategories(AcceptanceTester $I)
     {
-		$I->amOnPage('');
+		foreach ($this->getDataForCategory() as $el) {
+			$I->amOnPage('');
+			//$I->waitForElementClickable(Categories::$categoryType);
+			$I->click($this->categories[$el]);
+			
+			//$I->seeInCurrentUrl($data['url']);
+			//$I->seeElement('#product > div');
+			//$I->see($data['header']);
+		}
+		
+
     }
+	
+	
+	protected function getDataForCategory()
+	{
+		return array_rand($this->categories, 2);
+	}
 }
